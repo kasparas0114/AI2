934f70cadf8f211b44e644d97d62bf877e2c9eb2
Vytautas <atgrupe@gmail.com>
2016-04-03 00:50:43 +0100

    m

---
+++ AI2/AI2/ResultMinMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI2
+{
+    class ResultMinMax
+    {
+        public string[] matrix;
+        public int score;
+        public int depth;
+        public ResultMinMax(string[] matrix, int score, int depth)
+        {
+            this.matrix = matrix;
+            this.score = score;
+            this.depth = depth;
+        }
+        public void updateMatrix(string[] matrix)
+        {
+            this.matrix = matrix;
+        }
+        public int getScore()
+        {
+            return score;
+        }
+        public int getIntrus()
+        {
+            for (int i = 0; i < 9; i++)
+                if (matrix[i].Equals("o"))
+                    return i;
+            return -1;
+        }
+    }
+}
