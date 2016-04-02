90b1c0aa3f77e88b0d71823856da5ad35b516812
kasparas0114 <kasparas.stadalnikas@gmail.com>
2016-04-02 18:26:38 +0300

    Dizainas pradinis

--- AI2/AI2/Form1.Designer.cs
+++ AI2/AI2/Form1.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(97, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(97, 53);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select team";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Algorithm";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 65);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(209, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 65);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 65);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(113, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 65);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(209, 206);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 65);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(304, 206);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(71, 65);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(113, 286);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(71, 65);
+            this.button7.TabIndex = 10;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(209, 286);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(71, 65);
+            this.button8.TabIndex = 11;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(304, 286);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(71, 65);
+            this.button9.TabIndex = 12;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(410, 220);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(186, 28);
+            this.button10.TabIndex = 13;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 423);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- AI2/AI2/Form1.cs
+++ AI2/AI2/Form1.cs
@@ -15,6 +15,52 @@ namespace AI2
         public Form1()
         {
             InitializeComponent();
+            InitializeCombobox();
+        }
+
+        public void HideGame()
+        {
+            var Buttons = ButtonsList();
+            foreach (var b in Buttons)
+                b.Hide();
+        }
+        public void ShowGame()
+        {
+            var Buttons = ButtonsList();
+            foreach (var b in Buttons)
+                b.Show();
+        }
+        public List<Button> ButtonsList()
+        {
+            var GameButtons = new List<Button>();
+            GameButtons.Add(button1);
+            GameButtons.Add(button2);
+            GameButtons.Add(button3);
+            GameButtons.Add(button4);
+            GameButtons.Add(button5);
+            GameButtons.Add(button6);
+            GameButtons.Add(button7);
+            GameButtons.Add(button8);
+            GameButtons.Add(button9);
+            return GameButtons;
+
+        }
+        public void InitializeCombobox ()
+        {
+            comboBox1.Items.Add(new ComboBoxItem("X", 0));
+            comboBox1.Items.Add(new ComboBoxItem("Y", 1));
+            comboBox2.Items.Add(new ComboBoxItem("Mini-Max", 0));
+            comboBox2.Items.Add(new ComboBoxItem("Alpha-Beta", 1));
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            var team = comboBox1.SelectedItem.ToString();
+            var alg = comboBox2.SelectedItem.ToString();
+            if (team == "X" && alg != "MiniMax")
+            {
+                
+            }
         }
     }
 }
